9f908daaa137d1c0b0d1b8bd7b2bc5b3236a4b8b
PaulaGaribay <paula.garibay9808@gmail.com>
2025-08-19 20:10:25 -0600

    version 1 aplicación con histogramas y gráficos de dispersión

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # proyecto_sprint_7
 Código para proyecto sprint 7
+
+Esta es una aplicación web desarrollada con Streamlit que permite analizar datos de anuncios de venta de vehículos en Estados Unidos.
+
+## Funcionalidades
+
+- Visualización de histogramas para explorar la distribución de los datos.
+- Gráficos de dispersión para observar relaciones entre variables.
+- Interfaz sencilla e interactiva, basada en casillas de verificación.
+
+## 📁 Cómo usar la aplicación
+
+1. Clona este repositorio 
+2. Asegúrate de tener instalado Python y los paquetes necesarios.
+3. Instala las dependencias
+4. Puedes ejecutar la aplicación localmente con streamlit run app.py
--- app.py
+++ app.py
@@ -1,1 +1,33 @@
 import pandas as pd
+import streamlit as st
+import plotly as pl
+import plotly.express as px
+
+car_data = pd.read_csv('vehicles_us.csv')
+
+st.header("Proyecto Sprint 7")
+st.dataframe(car_data)
+
+build_histogram = st.checkbox('Construir un histograma')
+
+if build_histogram:
+    st.write('Construyendo histograma para la columna odómetro...')
+
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer", title='Histograma del odómetro')
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+# Casilla para construir gráfico de dispersión
+build_scatter = st.checkbox('Construir un gráfico de dispersión')
+
+if build_scatter:
+    st.write('Construyendo gráfico de dispersión: precio vs odómetro...')
+
+    # crear un histograma
+    fig2 = px.scatter(car_data, x="odometer", y='price',
+                      title='Relación entre odómetro y precio')
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig2, use_container_width=True)
